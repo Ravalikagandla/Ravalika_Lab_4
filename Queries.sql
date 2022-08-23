243754d5faaeb74acebcd6d65c053b7422f93e50
Ravalika08 <107806397+Ravalikagandla@users.noreply.github.com>
2022-08-23 13:48:21 +0530

    Add files via upload

---
+++ Queries.sql
@@ -0,0 +1,57 @@
+ 
+3rd Query
+
+NoOfCustomers,cus_gender
+2,M
+1,F
+ 
+4th Query
+
+pro_name,ORD_ID,ORD_AMOUNT,ORD_DATE,CUS_ID,PRICING_ID
+"GTA V",101,1500,2021-10-06,2,1
+TSHIRT,110,2500,2021-09-10,2,4
+MILK,116,99,2021-09-17,2,14
+ 
+5th Query
+
+SUPP_ID,SUPP_NAME,SUPP_CITY,SUPP_PHONE
+1,"Rajesh Retails",Delhi,1234567890
+2,"Appario Ltd.",Mumbai,2589631470
+4,"Bansal Retails",Kochi,8975463285
+5,"Mittal Ltd.",Lucknow,7898456532
+ 
+6th Query
+
+CAT_ID,CAT_NAME,Min_Price,pro_name
+2,GAMES,1450,"GTA V"
+5,CLOTHES,2500,TSHIRT
+4,ELECTRONICS,529,"ROG LAPTOP"
+3,GROCERIES,99,OATS
+1,BOOKS,780,"HARRY POTTER"
+
+
+7th Query
+
+PRO_ID,PRO_NAME
+1,"GTA V"
+4,OATS
+ 
+ 
+8th Query
+
+CUS_NAME,cus_gender
+AAKASH,M
+AMAN,M
+NEHA,F
+MEGHA,F
+ 
+9th Query 
+
+supp_id,supp_name,Average,Type_of_Service
+1,"Rajesh Retails",2.1667,"Average Service"
+2,"Appario Ltd.",3.3333,"Average Service"
+3,"Knome products",5.0000,"Excellent Service"
+4,"Bansal Retails",4.0000,"Average Service"
+5,"Mittal Ltd.",2.0000,"Poor Service"
+ 
+
